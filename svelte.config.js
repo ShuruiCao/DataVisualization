50007c3055f63649ced4be96d0354fe4d1eb9a65
ShuruiCao <sherry.z.cao@gmail.com>
2023-04-29 17:04:58 -0400

    config

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({fallback: 'index.html'}),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		  }),
 		paths: {
 			base: '/DataVisualization',
-			assets: '/DataVisualization/src/_app'
+			assets: '/DataVisualization/'
 		},
 		
 	}
